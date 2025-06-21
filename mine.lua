53da1a6c2d81fbeec4bcb4fdc60d5cfd2ca0b1a2
SINNAGAMI <lakenpeppard911@gmail.com>
2025-06-21 11:50:41 +0100

    Update mine.lua

--- mine.lua
+++ mine.lua
@@ -32,11 +32,11 @@ EnableRCS = true --- When Set to false, it'll turn off Recoil Control!
 
 RecoilControlMode = "High" --- Presets: "Low", "Medium", "High", "Ultra", "Insanity", "Custom"
 
-RcCustomStrength = 7 -- Value MUST be ROUNDED! No Decimal values such as 6.5!
+RcCustomStrength = 20 -- Value MUST be ROUNDED! No Decimal values such as 6.5!
 
 RequireToggle = true -- Change to false if you want it to always be on.
 
-ToggleKey = "CapsLock" -- Usable Keys: "CapsLock", "NumLock", "ScrollLock"
+ToggleKey = "numlock" -- Usable Keys: "CapsLock", "NumLock", "ScrollLock"
 
 DelayRate = 7 -- DO NOT CHANGE IF YOU DON'T KNOW WHAT YOU'RE DOING.
 
@@ -73,13 +73,13 @@ DelayRate = 7 -- DO NOT CHANGE IF YOU DON'T KNOW WHAT YOU'RE DOING.
 
 -------------  Recoil Presets ------
 if RecoilControlMode == "Low" then
-  RecoilControlStrength = 2
-elseif RecoilControlMode == "Medium" then
-  RecoilControlStrength = 6
-elseif RecoilControlMode == "High" then
-  RecoilControlStrength = 8
-elseif RecoilControlMode == "Ultra" then
   RecoilControlStrength = 12
+elseif RecoilControlMode == "Medium" then
+  RecoilControlStrength = 15
+elseif RecoilControlMode == "High" then
+  RecoilControlStrength = 20
+elseif RecoilControlMode == "Ultra" then
+  RecoilControlStrength = 25
 elseif RecoilControlMode == "Insanity" then
   RecoilControlStrength = 31
 elseif RecoilControlMode == "Custom" then
